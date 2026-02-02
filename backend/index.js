1a6f3506b9805d1b6117bd332817e2550971f7a3
Shree-vathsan <shreevathsan1904@gmail.com>
2026-02-02 23:06:46 +0530

    Day 2 CRUD.

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import supabase from './supabase.js';
 
 const app =express()
-app.use(express.json());
+app.use(express.json())
 
 app.get('/todos',async(req,res)=>{
     const{data, error} = await supabase
@@ -17,6 +17,53 @@ app.get('/todos',async(req,res)=>{
 })
 
 
+app.post('/todos',async(req,res)=>{
+    const {title} = req.body;
+
+    if(!title){
+        return res.status(400).json({error:"title required"})
+    }
+
+    const {data, error} = await supabase.from('todos').insert([{title}]).select()
+
+    if (error){
+        return res.status(500).json({error:error.message})
+    }
+
+    res.status(200).json(data[0])
+})
+
+app.put('/todos/:id', async(req,res)=>{
+    const {id} = req.params
+    const {completed} = req.body
+
+    const {data,error} = await supabase
+    .from ('todos')
+    .update({completed})
+    .eq('id',id)
+    .select()
+
+    if(error){
+        return res.status(500).json({error: error.message})
+    }
+
+    res.json(data[0])
+})
+
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params
+
+  const { error } = await supabase
+    .from('todos')
+    .delete()
+    .eq('id', id)
+
+  if (error) {
+    return res.status(500).json({ error: error.message })
+  }
+
+  res.json({ message: 'deleted' })
+})
 
 app.listen(5000,()=>{
     {
